610e10cf25e3c93c961188e15ba1f86bb3d5501e
mannyzhao <manny_zhao@163.com>
2018-11-14 09:30:51 +0800

    首页布局完成

--- Manny_Ec/src/main/java/com/zhaoman/manny_ec/main/index/IndexDelegate.java
+++ Manny_Ec/src/main/java/com/zhaoman/manny_ec/main/index/IndexDelegate.java
@@ -2,10 +2,18 @@ package com.zhaoman.manny_ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.zhaoman.manny_core.delegates.bottom.BottomItemDelagate;
 import com.zhaoman.manny_ec.R;
+import com.zhaoman.manny_ec.R2;
+
+import butterknife.BindView;
 
 /**
  * Author:zhaoman
@@ -13,6 +21,19 @@ import com.zhaoman.manny_ec.R;
  * Description:
  */
 public class IndexDelegate extends BottomItemDelagate{
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView=null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout=null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToobar=null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView=null;
+
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
