1848e7141c773d5199dd99254409a3ce60bd1c48
zhaoman <manny_zhao@163.com>
2019-10-10 22:17:20 +0800

    webview 封装完成

--- Manny_core/src/main/java/com/zhaoman/manny_core/delegates/web/WebViewInitializer.java
+++ Manny_core/src/main/java/com/zhaoman/manny_core/delegates/web/WebViewInitializer.java
@@ -1,7 +1,9 @@
 package com.zhaoman.manny_core.delegates.web;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -11,6 +13,16 @@ public class WebViewInitializer {
     public WebView createView(WebView webView){
         //设置可调试
         webView.setWebContentsDebuggingEnabled(true);
+
+        //cookie
+        final CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(webView,true);
+        }
+        CookieManager.setAcceptFileSchemeCookies(true);
+
+
         //不能横向滚动
         webView.setHorizontalScrollBarEnabled(false);
         //不能纵向滚动
